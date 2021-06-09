e71dd33c13785707186724e9e66defcfe7bf4643
Paolo Jovon <paolo.jovon@gmail.com>
2021-06-09 18:12:24 +0100

    Add some getters for Texture

--- libktx-rs/src/texture.rs
+++ libktx-rs/src/texture.rs
@@ -21,8 +21,45 @@ impl<'a> Texture<'a> {
         source.create_texture()
     }
 
+    /// Returns the pointer to the (C-allocated) underlying [`sys::ktxTexture`].
+    ///
+    /// **SAFETY**: Pointers are harmless. Dereferencing them is not!
     pub fn handle(&self) -> *mut sys::ktxTexture {
         self.handle
+    }
+
+    /// Returns the total size of image data, in bytes.
+    pub fn data_size(&self) -> usize {
+        // SAFETY: Safe if `self.handle` is sane.
+        unsafe { sys::ktxTexture_GetDataSize(self.handle) as usize }
+    }
+
+    /// Returns a read-only view on the image data.
+    pub fn data(&self) -> &[u8] {
+        let data = unsafe { sys::ktxTexture_GetData(self.handle) };
+        // SAFETY: Safe if `self.handle` is sane.
+        unsafe { std::slice::from_raw_parts(data, self.data_size()) }
+    }
+
+    /// Returns a read-write view on the image data.
+    pub fn data_mut(&mut self) -> &mut [u8] {
+        let data = unsafe { sys::ktxTexture_GetData(self.handle) };
+        // SAFETY: Safe if `self.handle` is sane.
+        unsafe { std::slice::from_raw_parts_mut(data, self.data_size()) }
+    }
+
+    /// Returns the pitch (in bytes) of an image row at the specified image level.  
+    /// This is rounded up to 1 if needed.
+    pub fn row_pitch(&self, level: u32) -> usize {
+        // SAFETY: Safe if `self.handle` is sane.
+        //         `level` is not used for indexing internally; no bounds-checking required.
+        unsafe { sys::ktxTexture_GetRowPitch(self.handle, level) as usize }
+    }
+
+    /// Returns the size (in bytes) of an element of the image.
+    pub fn element_size(&self) -> usize {
+        // SAFETY: Safe if `self.handle` is sane.
+        unsafe { sys::ktxTexture_GetElementSize(self.handle) as usize }
     }
 }
 
